39dfe6577bd8972dc6abb0811f36b9388c45bdda
zeshan <xeshan.ahmed@gmail.com>
2018-11-22 19:57:48 +0500

    bulls and cows

--- misc/bulls-and-cows.py
+++ misc/bulls-and-cows.py
@@ -1,3 +1,9 @@
+##Randomly generate a 4-digit number. Ask the user to guess a 4-digit number. For every digit that the user guessed correctly
+##in the correct place, they have a “cow”. For every digit the user guessed correctly in the wrong place is a “bull.” Every
+##time the user makes a guess, tell them how many “cows” and “bulls” they have. Once the user guesses the correct number, the
+##game is over. Keep track of the number of guesses the user makes throughout teh game and tell the user at the end.
+
+
 import random 
 def vt (g, n): # g : string and n string  
 	t=v=0
